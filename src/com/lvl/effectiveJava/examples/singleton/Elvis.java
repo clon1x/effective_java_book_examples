23ac20d63e093f97adc301809fe4d0ae8a90101d
LVL <luis.vinelerma@telefonica.com>
2023-05-10 15:05:41 +0200

    Example of Serializable Singleton
    Implement readResolve to preserve Singleton property.

--- src/com/lvl/effectiveJava/examples/singleton/Elvis.java
+++ src/com/lvl/effectiveJava/examples/singleton/Elvis.java
@@ -1,13 +1,19 @@
 package com.lvl.effectiveJava.examples.singleton;
 
+import java.io.Serializable;
+
 /**
  * Singleton with public final field
  * 
  * @author LVL
  *
  */
-public class Elvis {
+public class Elvis implements Serializable {
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1300973988232000629L;
 	private static final Elvis INSTANCE = new Elvis();
 	
 	private Elvis() {}
@@ -18,6 +24,17 @@ public class Elvis {
 
 	public void sing() {
 		System.out.println("Elvis sings a beautiful song");
+	}
+	
+	/**
+	 * Implement readResolve to preserve Singleton property on deserialization.
+	 * Return the true and only instance and let the garbage collector take 
+	 * care of this object.
+	 * 
+	 * @return The singleton Instance.
+	 */
+	private Object readResolve() {
+		return INSTANCE;
 	}
 
 }
